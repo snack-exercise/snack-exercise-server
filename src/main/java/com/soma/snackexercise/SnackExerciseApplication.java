4f63b6b707399a76f0a0f96e03d9c365d0521c4b
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2023-07-03 20:42:31 +0900

    initial commit (#7)
    프로젝트 생성

---
+++ src/main/java/com/soma/snackexercise/SnackExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.soma.snackexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnackExerciseApplication.class, args);
+    }
+
+}
