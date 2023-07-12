dc7e522c858d37f12fad04a71a572de44a1fcea9
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-12 16:23:49 +0900

    fix : @RestController 추가 (#8)

--- src/main/java/com/soma/snackexercise/controller/HealthController.java
+++ src/main/java/com/soma/snackexercise/controller/HealthController.java
@@ -1,7 +1,9 @@
 package com.soma.snackexercise.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HealthController {
     @GetMapping("/health")
     public String healthCheck(){
