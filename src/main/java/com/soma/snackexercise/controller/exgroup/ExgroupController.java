cb66d8eefdef98580bd1edd9613343757ee29a45
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-16 23:32:43 +0900

    fix : @AuthenicationPrincipal 수정 (#22)

--- src/main/java/com/soma/snackexercise/controller/exgroup/ExgroupController.java
+++ src/main/java/com/soma/snackexercise/controller/exgroup/ExgroupController.java
@@ -1,24 +1,23 @@
 package com.soma.snackexercise.controller.exgroup;
 
 
-import com.soma.snackexercise.auth.oauth.CustomOAuth2User;
 import com.soma.snackexercise.dto.exgroup.PostCreateExgroupRequest;
 import com.soma.snackexercise.service.exgroup.ExgroupService;
 import com.soma.snackexercise.util.response.Response;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class ExgroupController {
 
     private final ExgroupService exGroupService;
-
-    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/api/exgroups")
-    public Response createGroup(@RequestBody PostCreateExgroupRequest groupCreateRequest, @AuthenticationPrincipal CustomOAuth2User loginUser){
-        return Response.success(exGroupService.createGroup(groupCreateRequest, loginUser.getEmail()));
+    public Response createGroup(@RequestBody PostCreateExgroupRequest groupCreateRequest, @AuthenticationPrincipal UserDetails loginUser){
+        return Response.success(exGroupService.createGroup(groupCreateRequest, loginUser.getUsername()));
     }
 }
