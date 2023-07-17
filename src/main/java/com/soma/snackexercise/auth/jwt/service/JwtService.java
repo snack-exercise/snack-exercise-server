2d103e0bb797506d5ae24e1853685a35f004f958
ohjinseo <ojs835@naver.com>
2023-07-18 03:40:14 +0900

    feat : 토큰 유효 검증에서 블랙리스트 검증 기능 및 편의 메소드 추가 (#38)

--- src/main/java/com/soma/snackexercise/auth/jwt/service/JwtService.java
+++ src/main/java/com/soma/snackexercise/auth/jwt/service/JwtService.java
@@ -2,7 +2,15 @@ package com.soma.snackexercise.auth.jwt.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.soma.snackexercise.auth.jwt.util.PasswordUtil;
+import com.soma.snackexercise.domain.member.Member;
+import com.soma.snackexercise.exception.ExpiredJwtException;
+import com.soma.snackexercise.exception.InvalidRefreshTokenException;
+import com.soma.snackexercise.exception.UnauthorizedException;
 import com.soma.snackexercise.repository.member.MemberRepository;
+import com.soma.snackexercise.util.RedisUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
@@ -10,6 +18,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.JwtDecoderInitializationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -42,7 +58,8 @@ public class JwtService {
     private static final String BEARER = "Bearer ";
 
     private final MemberRepository memberRepository;
-    private final RedisTemplate<String, String> redisTemplate;
+    private final RedisUtil redisUtil;
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
 
     public String createAccessToken(String email) {
         Date now = new Date();
@@ -133,7 +150,7 @@ public class JwtService {
     RefreshToken DB 저장 (업데이트)
      */
     public void updateRefreshToken(String email, String refreshToken) {
-        redisTemplate.opsForValue().set(email, refreshToken);
+        redisUtil.set(email, refreshToken, refreshTokenExpirationPeriod);
     }
 
     /*
@@ -142,10 +159,61 @@ public class JwtService {
     public boolean isTokenValid(String token) {
         try {
             JWT.require(Algorithm.HMAC512(secretKey)).build().verify(token);
+
+            if (redisUtil.hasKeyBlackList(token)) {
+                throw new UnauthorizedException("이미 로그아웃한 회원입니다");
+            }
+
             return true;
+        } catch (SecurityException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (JWTDecodeException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        } catch (UnauthorizedException e) {
+            log.info("이미 탈퇴한 회원입니다.");
         } catch (Exception e) {
-            log.error("유효하지 않은 토큰입니다. {}", e.getMessage());
-            return false;
+            log.info("잘못된 JWT 토큰입니다.");
         }
+        return false;
+    }
+
+    public boolean isRefreshTokenMatch(String refreshToken) {
+        String email = extractEmail(refreshToken).orElseThrow(InvalidRefreshTokenException::new);
+        if (redisUtil.get(email).equals(refreshToken)) {
+            return true;
+        }
+
+        throw new InvalidRefreshTokenException();
+    }
+
+    public void sendAccessAndRefreshByEmail(HttpServletResponse response, String email) {
+        String AT = createAccessToken(email);
+        String RT = createRefreshToken(email);
+        updateRefreshToken(email, RT);
+        sendAccessAndRefreshToken(response, AT, RT);
+    }
+
+    public void saveAuthentication(Member myMember) {
+        log.info("JwtService.saveAuthentication() 호출");
+        String password = myMember.getPassword();
+
+        if (password == null) {
+            password = PasswordUtil.generateRandomPassword();
+        }
+
+        UserDetails userDetailsUser = User.builder()
+                .username(myMember.getEmail())
+                .password(password)
+                .roles(myMember.getRole().name())
+                .build();
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetailsUser, null,
+                authoritiesMapper.mapAuthorities(userDetailsUser.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
