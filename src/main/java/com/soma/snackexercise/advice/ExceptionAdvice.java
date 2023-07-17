e3a6256c405cda760f35b08ff6ecba76fa22e368
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-18 01:13:34 +0900

    feat : 존재하지 않는 운동 그룹 에러 처리 (#33)

--- src/main/java/com/soma/snackexercise/advice/ExceptionAdvice.java
+++ src/main/java/com/soma/snackexercise/advice/ExceptionAdvice.java
@@ -1,7 +1,16 @@
 package com.soma.snackexercise.advice;
 
+import com.soma.snackexercise.exception.ExgroupNotFoundException;
+import com.soma.snackexercise.util.response.Response;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.soma.snackexercise.advice.ErrorCode.NOT_FOUND_EXGROUP;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
+    @ExceptionHandler(ExgroupNotFoundException.class)
+    public Response exgroupNotFoundExceptionHandler(ExgroupNotFoundException e){
+        return Response.failure(NOT_FOUND_EXGROUP);
+    }
 }
