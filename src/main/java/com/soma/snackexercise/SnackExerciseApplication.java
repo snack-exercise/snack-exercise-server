c59ba8e1d4a21528bd78e1580ae17581661521bc
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-22 17:13:19 +0900

    fix : swagger cors 에러 해결

--- src/main/java/com/soma/snackexercise/SnackExerciseApplication.java
+++ src/main/java/com/soma/snackexercise/SnackExerciseApplication.java
@@ -1,9 +1,12 @@
 package com.soma.snackexercise;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://dev.snackexercise.com")})
 @EnableJpaAuditing
 @SpringBootApplication
 public class SnackExerciseApplication {
