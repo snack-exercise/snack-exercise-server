f363b6295e3944196ce620ffbe20b3c1d7e21886
ohjinseo <ojs835@naver.com>
2023-07-24 17:14:44 +0900

    fix : 시작일/종료일 필드 제거, 시간 필드에 json 직렬화 설정 (#70)

--- src/main/java/com/soma/snackexercise/dto/exgroup/response/ExgroupResponse.java
+++ src/main/java/com/soma/snackexercise/dto/exgroup/response/ExgroupResponse.java
@@ -1,11 +1,12 @@
 package com.soma.snackexercise.dto.exgroup.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 
 
@@ -13,7 +14,7 @@ import java.time.LocalTime;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class GetOneExgroupResponse {
+public class ExgroupResponse {
 
     private String name; // 그룹 이름
 
@@ -27,8 +28,10 @@ public class GetOneExgroupResponse {
 
     private Integer goalRelayNum; // 목표 릴레이 횟수
 
+    @JsonSerialize(using = LocalTimeSerializer.class)
     private LocalTime startTime; // 시작 시간
 
+    @JsonSerialize(using = LocalTimeSerializer.class)
     private LocalTime endTime; // 종료 시간
 
     private String penalty; // 벌칙
@@ -40,8 +43,4 @@ public class GetOneExgroupResponse {
     private Integer checkIntervalTime; // 미션 수행 체크 시간 간격
 
     private Integer checkMaxNum; // 일별 미션 수행 체크 최대 횟수
-
-    private LocalDate startDate; // 시작 날짜
-
-    private LocalDate endDate; // 종료 날짜
 }
