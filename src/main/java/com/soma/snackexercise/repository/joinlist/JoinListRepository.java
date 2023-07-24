162fe859cc884ad255bd42efc191c2f6a048843f
ohjinseo <ojs835@naver.com>
2023-07-25 01:59:59 +0900

    feat : joinList 그룹 존재 여부, 그룹에 속한 가장 오래된 회원의 joinList 찾는 쿼리 메소드 추가 (#78)

--- src/main/java/com/soma/snackexercise/repository/joinlist/JoinListRepository.java
+++ src/main/java/com/soma/snackexercise/repository/joinlist/JoinListRepository.java
@@ -13,7 +13,12 @@ import java.util.Optional;
 public interface JoinListRepository extends JpaRepository<JoinList, Long> {
     void deleteByMember(Member member);
 
+    Boolean existsByExgroupAndStatus(Exgroup exgroup, Status status);
+
     Optional<JoinList> findByExgroupAndMemberAndStatus(Exgroup exgroup, Member member, Status status);
+
+    // exgroup와 status로 JoinList를 찾고, createdAt으로 오름차순 정렬하여 가장 첫번째 요소 반환
+    Optional<JoinList> findFirstByExgroupAndStatusOrderByCreatedAtAsc(Exgroup exgroup, Status status);
 
     Boolean existsByExgroupAndMemberAndJoinTypeAndStatus(Exgroup exgroup, Member member, JoinType joinType, Status status);
 
