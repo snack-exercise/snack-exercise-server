0692f325d054ca0c6cd477b533b0b43713eea707
ohjinseo <ojs835@naver.com>
2023-07-24 18:24:44 +0900

    feat : 그룹의 방장 여부, 그룹 인원 수 세는 쿼리 메소드 작성 (#70)

--- src/main/java/com/soma/snackexercise/repository/exgroup/ExgroupRepository.java
+++ src/main/java/com/soma/snackexercise/repository/exgroup/ExgroupRepository.java
@@ -1,8 +1,13 @@
 package com.soma.snackexercise.repository.exgroup;
 
 import com.soma.snackexercise.domain.exgroup.Exgroup;
+import com.soma.snackexercise.util.constant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface ExgroupRepository extends JpaRepository<Exgroup, Long> {
     Boolean existsByCode(String code);
+
+    Optional<Exgroup> findByIdAndStatus(Long id, Status status);
 }
