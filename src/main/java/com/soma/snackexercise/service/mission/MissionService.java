a6cf8bdce134f69d589eaa0c3ba0c9a309129dfa
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-24 15:43:46 +0900

    feat : 릴레이 미션 오늘날짜 Ranking Service 추가 (#68)

--- src/main/java/com/soma/snackexercise/service/mission/MissionService.java
+++ src/main/java/com/soma/snackexercise/service/mission/MissionService.java
@@ -1,8 +1,10 @@
 package com.soma.snackexercise.service.mission;
 
 import com.soma.snackexercise.domain.exgroup.Exgroup;
+import com.soma.snackexercise.domain.member.Member;
 import com.soma.snackexercise.domain.mission.Mission;
 import com.soma.snackexercise.dto.mission.response.MemberMissionDto;
+import com.soma.snackexercise.dto.mission.response.RankingResponseDto;
 import com.soma.snackexercise.dto.mission.response.TodayMissionCurrentResultDto;
 import com.soma.snackexercise.exception.ExgroupNotFoundException;
 import com.soma.snackexercise.repository.exgroup.ExgroupRepository;
@@ -12,7 +14,10 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
@@ -20,7 +25,6 @@ public class MissionService {
 
     private final MisssionRepository misssionRepository;
     private final ExgroupRepository exgroupRepository;
-
 
     public TodayMissionCurrentResultDto getTodayMissionResults(Long exgroupId) {
         // 1. 그룹의 종료일자
@@ -38,5 +42,44 @@ public class MissionService {
         List<MemberMissionDto> missionFlow = missions.stream().map(mission -> new MemberMissionDto(mission.getMember().getId(), mission.getMember().getNickname(), mission.getMember().getProfileImage(), mission.getCreatedAt(), mission.getEndAt())).toList();
 
         return new TodayMissionCurrentResultDto(missionFlow, currentFinishedRelayCount, exgroup.getEndDate());
+    }
+
+    public Object getTodayMissionRank(Long exgroupId) {
+        Exgroup exgroup = exgroupRepository.findById(exgroupId).orElseThrow(ExgroupNotFoundException::new); // 존재하는 exgroupId인지 검증
+
+        // 1. 오늘 날짜의 모든 미션 조회
+        LocalDateTime now = LocalDateTime.now().minusDays(1);// 현재 날짜와 시간 가져오기
+        LocalDateTime todayMidnight = now.with(LocalTime.MIN);// 오늘 자정 구하기
+        LocalDateTime tomorrowMidnight = now.plusDays(1).with(LocalTime.MIN);// 내일 자정 구하기
+
+        List<Mission> missions = misssionRepository.findAllExecutedMissionByGroupIdAndCreatedAt(exgroupId, todayMidnight, tomorrowMidnight);
+
+        return getRankingList(missions);
+    }
+
+    private static List<RankingResponseDto> getRankingList(List<Mission> missions) {
+        // 1. memberId 별로 평균 속도 계산
+        Map<Long, RankingResponseDto> todayRankingMap = new HashMap<>();
+
+        for (Mission mission : missions) {
+            Long memberId = mission.getMember().getId();
+            long minutesDiffBetweenCreateAndStart = ChronoUnit.MINUTES.between(mission.getCreatedAt(), mission.getStartAt());
+
+            if(!todayRankingMap.containsKey(memberId)){
+                Member member = mission.getMember();
+                todayRankingMap.put(memberId, new RankingResponseDto(member.getName(), member.getProfileImage(), minutesDiffBetweenCreateAndStart, 1));
+            }else{
+                todayRankingMap.get(memberId).addTime(minutesDiffBetweenCreateAndStart);
+            }
+        }
+
+        // 2. 평균 속도 계산
+        todayRankingMap.forEach((k, v) -> v.calcAvgTime());
+
+        // 3. 평균 속도 기준 오름차순 정렬
+        List<RankingResponseDto> rankingList = todayRankingMap.values().stream()
+                .sorted((a, b) -> (int) (a.getAvgMissionExecutionTime() - b.getAvgMissionExecutionTime())).toList();
+
+        return rankingList;
     }
 }
