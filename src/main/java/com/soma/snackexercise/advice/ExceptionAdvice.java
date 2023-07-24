b84d5c57cf67a91bc4a3c46a41b48e0ace0a2dfa
ohjinseo <ojs835@naver.com>
2023-07-24 10:42:45 +0900

    chore : 토큰 만료, 유저 이름 존재 에러 핸들러 작성 (#48)

--- src/main/java/com/soma/snackexercise/advice/ExceptionAdvice.java
+++ src/main/java/com/soma/snackexercise/advice/ExceptionAdvice.java
@@ -1,15 +1,38 @@
 package com.soma.snackexercise.advice;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.soma.snackexercise.exception.ExgroupNotFoundException;
+import com.soma.snackexercise.exception.MemberNameAlreadyExistsException;
 import com.soma.snackexercise.util.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import static com.soma.snackexercise.advice.ErrorCode.NOT_FOUND_EXGROUP;
+
+import static com.soma.snackexercise.advice.ErrorCode.*;
 
 @RestControllerAdvice
 @Slf4j
 public class ExceptionAdvice {
+    /*
+    Authorization
+     */
+    @ExceptionHandler(TokenExpiredException.class)
+    public Response tokenExpiredException(TokenExpiredException e) {
+        return Response.failure(TOKEN_EXPIRED_EXCEPTION);
+    }
+
+
+    /*
+    Member
+     */
+    @ExceptionHandler(MemberNameAlreadyExistsException.class)
+    public Response memberNameAlreadyExistsException(MemberNameAlreadyExistsException e) {
+        return Response.failure(MEMBER_NAME_ALREADY_EXISTS_EXCEPTION);
+    }
+
+    /*
+    Exgroup
+     */
     @ExceptionHandler(ExgroupNotFoundException.class)
     public Response exgroupNotFoundExceptionHandler(ExgroupNotFoundException e){
         return Response.failure(NOT_FOUND_EXGROUP);
