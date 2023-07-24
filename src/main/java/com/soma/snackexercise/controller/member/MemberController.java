68aa1cde6eed61937c3fb3905c1d3d419a68f433
ohjinseo <ojs835@naver.com>
2023-07-24 18:56:03 +0900

    feat : 멤버 수정 API 문서 설정 (#70)

--- src/main/java/com/soma/snackexercise/controller/member/MemberController.java
+++ src/main/java/com/soma/snackexercise/controller/member/MemberController.java
@@ -1,10 +1,15 @@
 package com.soma.snackexercise.controller.member;
 
 import com.soma.snackexercise.dto.member.request.MemberUpdateRequest;
+import com.soma.snackexercise.dto.member.response.MemberResponse;
 import com.soma.snackexercise.service.member.MemberService;
 import com.soma.snackexercise.util.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +36,13 @@ public class MemberController {
         return Response.success(memberService.getAllExgroupMembers(groupId));
     }
 
+    @Operation(summary = "멤버 수정",
+            description = "멤버 한명을 수정합니다.",
+            security = { @SecurityRequirement(name = "bearer-key") },
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "멤버 수정 성공", content = @Content(schema = @Schema(implementation = MemberResponse.class)))
+            })
+    @Parameter(name = "memberId", description = "수정할 멤버 ID", required = true,  in = ParameterIn.PATH)
     @PutMapping("/members/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Response update(@PathVariable Long id, @RequestBody MemberUpdateRequest request) {
