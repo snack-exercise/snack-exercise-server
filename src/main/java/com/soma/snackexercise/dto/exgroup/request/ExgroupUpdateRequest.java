64aee5f5c7f72929bbc3e2a55353c6bc03d90776
ohjinseo <ojs835@naver.com>
2023-07-24 18:25:32 +0900

    feat : exgroup update request DTO 작성 (#70)

---
+++ src/main/java/com/soma/snackexercise/dto/exgroup/request/ExgroupUpdateRequest.java
@@ -0,0 +1,40 @@
+package com.soma.snackexercise.dto.exgroup.request;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExgroupUpdateRequest {
+    private String name; // 그룹 이름
+
+    private String emozi; // 그룹 대표 이모지
+
+    private String color; // 그룹 대표 색상
+
+    private String description; // 그룹 소개 글
+
+    private Integer maxMemberNum; // 최대 참여 인원 수
+
+    private Integer goalRelayNum; // 목표 릴레이 횟수
+
+    @JsonDeserialize(using = LocalTimeDeserializer.class)
+    private LocalTime startTime; // 시작 시간
+
+    @JsonDeserialize(using = LocalTimeDeserializer.class)
+    private LocalTime endTime; // 종료 시간
+
+    private String penalty; // 벌칙
+
+    private Integer missionIntervalTime; // 미션 수행 시간 간격
+
+    private Integer checkIntervalTime; // 미션 수행 체크 시간 간격
+
+    private Integer checkMaxNum; // 일별 미션 수행 체크 최대 횟수
+}
