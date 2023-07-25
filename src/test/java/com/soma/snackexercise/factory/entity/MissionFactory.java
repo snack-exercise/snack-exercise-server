cb47b1081923d9b5f76f837e7458025dd286ef0c
ohjinseo <ojs835@naver.com>
2023-07-26 00:34:22 +0900

    feat : 미션 도메인 factory 메소드 생성 및 JPA 테스트 (#84)

---
+++ src/test/java/com/soma/snackexercise/factory/entity/MissionFactory.java
@@ -0,0 +1,29 @@
+package com.soma.snackexercise.factory.entity;
+
+import com.soma.snackexercise.domain.exercise.Exercise;
+import com.soma.snackexercise.domain.exgroup.Exgroup;
+import com.soma.snackexercise.domain.member.Member;
+import com.soma.snackexercise.domain.mission.Mission;
+
+public class MissionFactory {
+    public static Mission createNonCompleteMission(Exercise exercise, Member member, Exgroup exgroup) {
+        return Mission.builder()
+                .exercise(exercise)
+                .member(member)
+                .exgroup(exgroup)
+                .build();
+    }
+
+    public static Mission createCompleteMission(Exercise exercise, Member member, Exgroup exgroup) {
+        Mission mission = Mission.builder()
+                .exercise(exercise)
+                .member(member)
+                .exgroup(exgroup)
+                .build();
+
+        mission.startMission();
+        mission.endMission(3);
+
+        return mission;
+    }
+}
