aa7a57e15761d338306742c12e8444fc068dbe8f
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-26 14:14:29 +0900

    fix : CORS 에러 해결

--- src/main/java/com/soma/snackexercise/config/WebConfig.java
+++ src/main/java/com/soma/snackexercise/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:3000") // 허용할 출처
                 .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
+                .allowedHeaders("*")
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
