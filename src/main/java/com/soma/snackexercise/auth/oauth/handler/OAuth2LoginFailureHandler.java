ac9b3aa39e988f6d416cc908fdd16053246dc5aa
ohjinseo <ojs835@naver.com>
2023-07-27 10:44:22 +0900

    fix : Oauth 로그인 실패 핸들러 에러 출력 방식 변경 (#86)

--- src/main/java/com/soma/snackexercise/auth/oauth/handler/OAuth2LoginFailureHandler.java
+++ src/main/java/com/soma/snackexercise/auth/oauth/handler/OAuth2LoginFailureHandler.java
@@ -17,6 +17,6 @@ public class OAuth2LoginFailureHandler implements AuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
         response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         response.getWriter().write("소셜 로그인 실패! 서버 로그를 확인해주세요.");
-        log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.getMessage());
+        log.info("소셜 로그인에 실패했습니다. 에러 메시지 : {}", exception.fillInStackTrace());
     }
 }
