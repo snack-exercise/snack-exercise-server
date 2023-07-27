fc6309401786148a4c9e3f8a37601e2d854899d1
ohjinseo <ojs835@naver.com>
2023-07-27 10:49:28 +0900

    fix : Excontroller get uri 수정 (#86)

--- src/main/java/com/soma/snackexercise/controller/exgroup/ExgroupController.java
+++ src/main/java/com/soma/snackexercise/controller/exgroup/ExgroupController.java
@@ -47,8 +47,8 @@ public class ExgroupController {
 
     @Operation(summary = "하나의 운동 그룹에 속한 모든 회원 조회", description = "하나의 운동 그룹에 속한 모든 회원을 조회합니다.", security = { @SecurityRequirement(name = "bearer-key") })
     @Parameter(name = "groupId", description = "조회할 운동 그룹 ID")
-    @GetMapping("/exgroups/{groupId}/members")
-    public Response getAllExgroupMembers(@PathVariable("groupId") Long groupId){
+    @GetMapping("/{groupId}/members")
+    public Response getAllExgroupMembers정(@PathVariable("groupId") Long groupId){
         return Response.success(exGroupService.getAllExgroupMembers(groupId));
     }
 
