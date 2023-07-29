3d38d6f7474d7443ac24318d2559ca8a28655390
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-07-30 01:14:26 +0900

    feat : status로 그룹을 찾는 repository 함수 생성 (#49)

--- src/main/java/com/soma/snackexercise/repository/exgroup/ExgroupRepository.java
+++ src/main/java/com/soma/snackexercise/repository/exgroup/ExgroupRepository.java
@@ -4,10 +4,13 @@ import com.soma.snackexercise.domain.exgroup.Exgroup;
 import com.soma.snackexercise.util.constant.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExgroupRepository extends JpaRepository<Exgroup, Long> {
     Boolean existsByCode(String code);
 
     Optional<Exgroup> findByIdAndStatus(Long id, Status status);
+
+    List<Exgroup> findAllByStatus(Status status);
 }
