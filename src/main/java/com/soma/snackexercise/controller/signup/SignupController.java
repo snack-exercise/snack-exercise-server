058a33cf81fed3333bff2f08f8f7987742a9bfb1
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2023-08-01 18:24:03 +0900

    fix : SignupController.java

--- src/main/java/com/soma/snackexercise/controller/signup/SignupController.java
+++ src/main/java/com/soma/snackexercise/controller/signup/SignupController.java
@@ -3,23 +3,24 @@ package com.soma.snackexercise.controller.signup;
 import com.soma.snackexercise.dto.signup.SignupRequest;
 import com.soma.snackexercise.service.signup.SignupService;
 import com.soma.snackexercise.util.response.Response;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@CrossOrigin(originPatterns = "http://localhost:3000")
 public class SignupController {
     private final SignupService signupService;
 
     @PostMapping("/api/sign-up")
     @ResponseStatus(HttpStatus.CREATED)
-    public Response signup(@RequestBody SignupRequest request, HttpServletResponse response, @AuthenticationPrincipal UserDetails loginUser) {
-        signupService.signup(request, response, loginUser.getUsername());
+    public Response signup(@RequestBody SignupRequest signupRequest, HttpServletRequest request, HttpServletResponse response) {
+        signupService.signup(signupRequest, request, response);
         return Response.success();
     }
 }
