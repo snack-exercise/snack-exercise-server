22628c44cbe8a9a9567f94da73ccfbf7edc9e96a
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2023-08-01 17:08:02 +0900

    fix : SignupController.java

--- src/main/java/com/soma/snackexercise/controller/signup/SignupController.java
+++ src/main/java/com/soma/snackexercise/controller/signup/SignupController.java
@@ -8,13 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@CrossOrigin("*")
 public class SignupController {
     private final SignupService signupService;
 
