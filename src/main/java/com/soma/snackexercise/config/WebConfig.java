ba6392130b89d4937241bae2e4a2a611d16a5893
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2023-08-01 17:59:08 +0900

    fix : WebConfig

--- src/main/java/com/soma/snackexercise/config/WebConfig.java
+++ src/main/java/com/soma/snackexercise/config/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // CORS 정책을 적용할 URL 패턴
                 .allowedOrigins("http://localhost:3000") // 허용할 출처
-                .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP method
+                .allowedMethods("POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
-                .allowedHeaders("*")
+                .allowedHeaders("Content-Type")
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
 }
