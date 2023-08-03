63c2e72c4ba33f11602887ee606d3cdd607ef375
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-03 11:51:05 +0900

    feat : 미션 수행 완료 푸시알림 Enum 생성 (#112)

--- src/main/java/com/soma/snackexercise/domain/notification/NotificationMessage.java
+++ src/main/java/com/soma/snackexercise/domain/notification/NotificationMessage.java
@@ -6,7 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum NotificationMessage {
-    ALLOCATE("운동 미션 할당! 💪", "지금 당장 운동 미션을 확인하고 운동을 수행해보세요! 🔥");
+    ALLOCATE("운동 미션 할당! 💪", "지금 당장 운동 미션을 확인하고 운동을 수행해보세요! 🔥"),
+    GROUP_GOAL_ACHIEVE("그룹 목표 달성! 🙌", "그룹의 목표 릴레이 횟수를 모두 달성했어요! 👍🏻");
 
     private final String title;
     private final String body;
