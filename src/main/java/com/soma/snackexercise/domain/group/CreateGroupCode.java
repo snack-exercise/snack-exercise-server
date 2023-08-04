f07270803104eadfee25f60b3f4ccfbbb4b48c9f
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-04 10:52:10 +0900

    fix : 그룹 코드 숫자로만 구성되도록 수정

--- src/main/java/com/soma/snackexercise/domain/group/CreateGroupCode.java
+++ src/main/java/com/soma/snackexercise/domain/group/CreateGroupCode.java
@@ -5,13 +5,12 @@ import java.util.Random;
 public class CreateGroupCode {
 
     private static Random random = new Random();
+    private static int leftLimit = 48; // numeral '0'
+    private static int rightLimit = 57; // numeral '9'
+    private static int targetStringLength = 6;
 
     public static String createGroupCode(){
-        int leftLimit = 48; // numeral '0'
-        int rightLimit = 122; // letter 'z'
-        int targetStringLength = 6;
         return random.ints(leftLimit, rightLimit + 1)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97)) // 영어 필터링
                 .limit(targetStringLength)
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
