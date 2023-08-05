ef5c9819b35bbcf8fcfeb9322ef27909303ede01
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-05 18:50:31 +0900

    fix : EC2에서 FCM 초기화 못하는 에러 해결

---
+++ src/main/java/com/soma/snackexercise/config/FCMConfig.java
@@ -0,0 +1,50 @@
+package com.soma.snackexercise.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FCMConfig {
+    @Value("${fcm.key.path}")
+    private String FCM_PRIVATE_KEY_PATH;
+
+
+    // 메시징만 권한 설정
+    @Value("${fcm.key.scope}")
+    private String fireBaseScope;
+
+    // project ID
+    @Value("${fcm.key.id}")
+    private String projectId;
+
+    //fcm 기본 설정 진행
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        // initialize Admin SDK using OAuth 2.0 refresh token
+        InputStream serviceAccountFile = new ClassPathResource(FCM_PRIVATE_KEY_PATH).getInputStream();
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountFile)
+                        .createScoped(List.of(fireBaseScope)))
+                .setProjectId(projectId) // 필수는 아님
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp){
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
--- src/main/java/com/soma/snackexercise/service/notification/FirebaseCloudMessageService.java
+++ src/main/java/com/soma/snackexercise/service/notification/FirebaseCloudMessageService.java
@@ -1,56 +1,19 @@
 package com.soma.snackexercise.service.notification;
 
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class FirebaseCloudMessageService {
-
-    @Value("${fcm.key.path}")
-    private String FCM_PRIVATE_KEY_PATH;
-
-
-    // 메시징만 권한 설정
-    @Value("${fcm.key.scope}")
-    private String fireBaseScope;
-
-     //fcm 기본 설정 진행
-//    @PostConstruct
-//    public void init() {
-//        try {
-//            FirebaseOptions options = new FirebaseOptions.Builder()
-//                    .setCredentials(
-//                            GoogleCredentials
-//                                    .fromStream(new ClassPathResource(FCM_PRIVATE_KEY_PATH).getInputStream())
-//                                    .createScoped(List.of(fireBaseScope)))
-//                    .build();
-//
-//            if (FirebaseApp.getApps().isEmpty()) {
-//                FirebaseApp.initializeApp(options);
-//                log.info("Firebase application has been initialized");
-//            }
-//        } catch (IOException e) {
-//            log.error(e.getMessage());
-//            e.printStackTrace();
-//            // spring 뜰때 알림 서버가 잘 동작하지 않는 것이므로 바로 죽임
-//            throw new RuntimeException(e.getMessage());
-//        }
-//    }
-
-
+    private final FirebaseMessaging firebaseMessaging;
 
     /**
      * tokenList를 받아 여러 개의 기기로 알림 보내기
@@ -70,7 +33,7 @@ public class FirebaseCloudMessageService {
                 .build();
 
         try {
-            BatchResponse response = FirebaseMessaging.getInstance().sendMulticast(multicastMessage);
+            BatchResponse response = firebaseMessaging.sendMulticast(multicastMessage);
             if (response.getFailureCount() > 0) {
                 List<SendResponse> responses = response.getResponses();
                 List<String> failedTokens = new ArrayList<>();
@@ -105,7 +68,7 @@ public class FirebaseCloudMessageService {
                 .build();
 
         try {
-            FirebaseMessaging.getInstance().send(message);
+            firebaseMessaging.send(message);
         } catch (FirebaseMessagingException e) {
             log.info("FirebaseMessagingException: {}, fail token: {}", e.getMessage(), token);
         }
