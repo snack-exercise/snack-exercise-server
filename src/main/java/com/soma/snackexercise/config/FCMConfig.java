63ef3094c32768bba2d17f211de615760c3e161a
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-05 20:12:31 +0900

    fix : EC2에서 FCM 초기화 못하는 에러 해결

--- src/main/java/com/soma/snackexercise/config/FCMConfig.java
+++ src/main/java/com/soma/snackexercise/config/FCMConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
