8b280ed4794d0c941f6e78cc864fefd495e09a09
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-10 18:13:36 +0900

    feat : blue, green 배포를 위한 스크립트 파일

---
+++ scripts/switch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# nginx가 가리키는 스프링 port 수정
+echo "> 현재 구동중인 Port 확인"
+CURRENT_PROFILE=$(curl -s http://localhost/profile)
+
+if [ $CURRENT_PROFILE == blue ]
+then
+  IDLE_PORT=8081
+elif [ $CURRENT_PROFILE == green ]
+then
+  IDLE_PORT=8080
+else
+  echo "> 일치하는 Profile이 없습니다. Profile:$CURRENT_PROFILE"
+  echo "> 8080을 할당합니다."
+  IDLE_PORT=8080
+fi
+
+PROXY_PORT=$(curl -s http://localhost/profile)
+echo "> 현재 구동중인 Port: $PROXY_PORT"
+
+echo "> 전환할 Port : $IDLE_PORT"
+echo "> Port 전환"
+echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+echo "> Nginx Reload"
+sudo service nginx reload
