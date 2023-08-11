45becdc0bf35d86fe923ef4e9f418c2a9bf2b089
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-11 13:21:54 +0900

    feat : 로그인, 회원가입 시 FCM 토큰 Blank validation 추가

--- src/main/java/com/soma/snackexercise/dto/auth/MvpLoginRequest.java
+++ src/main/java/com/soma/snackexercise/dto/auth/MvpLoginRequest.java
@@ -1,5 +1,7 @@
 package com.soma.snackexercise.dto.auth;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +11,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MvpLoginRequest {
     private String nickname;
+
+    @NotBlank(message = "fcm 토큰은 필수 값입니다.")
     private String fcmToken;
 }
