2599d2b46de9a09041f154217b8502bb69da14bc
김민정 <62479120+mingsound21@users.noreply.github.com>
2023-08-12 01:19:11 +0900

    fix : deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,15 +3,15 @@
 echo "> 현재 구동중인 profile 확인"
 CURRENT_PROFILE=$(curl -s http://localhost/profiles)
 
-echo "> $CURRENT_PROFILE"
+echo "> CURRENT_PROFILE은 $CURRENT_PROFILE"
 
-if [ $CURRENT_PROFILE == "blue" ]
+if [ $CURRENT_PROFILE == blue ]
 then
-        IDLE_PROFILE="green"
+        IDLE_PROFILE=green
         IDLE_PORT=8081
-elif [ $CURRENT_PROFILE == "green" ]
+elif [ $CURRENT_PROFILE == green ]
 then
-        IDLE_PROFILE="blue"
+        IDLE_PROFILE=blue
         IDLE_PORT=8080
 else
         echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
@@ -22,11 +22,14 @@ fi
 
 CONTAINER_ID=$(docker container ls -f "name=spring-${IDLE_PROFILE}" -q)
 
-sudo docker stop spring-${IDLE_PROFILE}
-sudo docker rm spring-${IDLE_PROFILE}
+sudo docker-compose -f docker-compose-prod.yml down
+sudo docker-compose -f docker-compose-prod.yml pull spring-${IDLE_PROFILE}
+sudo docker rm -f snack-redis
+sudo docker rm -f spring-${IDLE_PROFILE}
+sudo docker image rm ojs835/snack-exercise-hub:latest-prod
 
 echo "> spring-$IDLE_PROFILE 컨테이너 배포"
-sudo docker-compose -f docker-compose-prod.yml up spring-${IDLE_PROFILE}
+sudo docker-compose -f docker-compose-prod.yml up -d spring-${IDLE_PROFILE}
 
 echo "> $IDLE_PROFILE 10초 후 Health check 시작"
 echo "> curl -s http://localhost:$IDLE_PORT/actuator/health "
